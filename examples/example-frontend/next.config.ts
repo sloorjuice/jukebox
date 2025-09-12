6c52c3408559a56a711ebcfe0c6aae90cdfc3a9d
sloorjuice <antant8085@gmail.com>
2025-09-12 10:31:33 -0400

    chore: try to fix

--- examples/example-frontend/next.config.ts
+++ examples/example-frontend/next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://jukebox.local:3000'
+    'http://jukebox.local:3000',
+    'http://jukebox.local:3000/'
   ],
 };
 
